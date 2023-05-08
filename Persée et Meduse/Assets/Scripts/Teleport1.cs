9509f23f5fcf03109ead8619a25c949d9b9cc4e1
camille.honnête <c.reuloscarrere@rubika-edu.com>
2023-05-08 19:59:20 +0200

    Final

---
+++ Persée et Meduse/Assets/Scripts/Teleport1.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class Teleport1 : MonoBehaviour
+{
+
+    public GameObject CanvasGroup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            CanvasGroup.GetComponent<CanvasGroup>().DOFade(1, 2);
+            StartCoroutine(wait());
+        }
+    }
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(2);
+    } 
+}
